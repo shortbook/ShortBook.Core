8ef97507a957e105e78c832f813913b169a912fd
gaoyunpeng <gaoyunpeng1982@gmail.com>
2019-08-25 14:12:32 +0900

    添加 将 Doamin 对象放到该库中。

---
+++ ShortBook.Core/Domain/AutoIdEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace ShortBook.Domain
+{
+    /// <summary>
+    /// 拥有自增ID的实体
+    /// </summary>
+    public abstract class AutoIdEntity
+    {
+        /// <summary>
+        /// 标识ID
+        /// </summary>
+        public long Id { get; set; }
+    }
+}
---
+++ ShortBook.Core/Domain/User/User.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using ShortBook.Properties;
+
+namespace ShortBook.Domain.User
+{
+    /// <summary>
+    /// 用户
+    /// </summary>
+    public class User : AutoIdEntity
+    {
+        /// <summary>
+        /// 名
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// 姓
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// 电子邮件
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// 登录口令
+        /// </summary>
+        public string Password { get; set; }
+
+        /// <summary>
+        /// 注册时间
+        /// </summary>
+        public DateTime LogonDate { get; set; }
+
+        /// <summary>
+        /// 验证用户信息
+        /// </summary>
+        public void Validate()
+        {
+            // TODO 验证用户信息
+        }
+
+        /// <summary>
+        /// 用户登录
+        /// </summary>
+        /// <param name="password"></param>
+        public void Login(string password)
+        {
+            var saltPassword = (password + Resources.Salt).Substring(0, 64);
+            using (SHA512 shaM = new SHA512Managed())
+            {
+                var hash = shaM.ComputeHash(Encoding.Unicode.GetBytes(saltPassword));
+                var hashedInputStringBuilder = new StringBuilder(128);
+                foreach (var b in hash)
+                {
+                    hashedInputStringBuilder.Append(b.ToString("X2"));
+                }
+
+                var sha512Password = hashedInputStringBuilder.ToString();
+                if (sha512Password != password)
+                {
+                    throw new ArgumentException("用户登录名或登录口令错误，请确认。");
+                }
+            }
+        }
+    }
+}
